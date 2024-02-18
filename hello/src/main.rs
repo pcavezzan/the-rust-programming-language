05a21c6d3637ecc33e658bc5f456d21bc366d188
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-02-18 23:36:45 +0100

    Building a Single-Threaded Web Server
    Refactoring and make it simpler.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -22,32 +22,17 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {:#?}", http_request);
     let request_line = http_request.get(0).unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let response = build_response(OK, "index.html");
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line ==  "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        let response = build_response(NOT_FOUND, "404.html");
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-}
+        ("HTTP/1.1 404 NOT_FOUND", "404.html")
+    };
 
-fn build_response(http_status: HttpStatus, html_file_path: &str) -> String {
-    let HttpStatus { status_code, status_name} = http_status;
-    let status_line = format!("HTTP/1.1 {status_code} {status_name}");
-    let contents = fs::read_to_string(html_file_path).unwrap();
-
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    let content_length = format!("Content-Length: {length}");
-    let headers = format!("{content_length}");
 
-    let response = format!("{status_line}\r\n{headers}\r\n\r\n{contents}");
-    response
-}
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-const OK: HttpStatus = HttpStatus { status_code: 200, status_name: "OK" };
-const NOT_FOUND: HttpStatus = HttpStatus { status_code: 404, status_name: "NOT_FOUND" };
-
-struct HttpStatus<'a> {
-    status_code: u64,
-    status_name: &'a str,
+    stream.write_all(response.as_bytes()).unwrap();
 }

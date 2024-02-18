ab7091abb17206906edc5b4a20b989c7964216d2
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-02-18 23:34:09 +0100

    Building a Single-Threaded Web Server
    Take care of route requested.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -17,17 +17,37 @@ fn handle_connection(mut stream: TcpStream) {
     let http_request: Vec<String> = buf_reader
         .lines()
         .map(|result| result.unwrap())
-        .take_while(|line|!line.is_empty())
+        .take_while(|line| !line.is_empty())
         .collect();
     println!("Request: {:#?}", http_request);
+    let request_line = http_request.get(0).unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    if request_line == "GET / HTTP/1.1" {
+        let response = build_response(OK, "index.html");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let response = build_response(NOT_FOUND, "404.html");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+}
+
+fn build_response(http_status: HttpStatus, html_file_path: &str) -> String {
+    let HttpStatus { status_code, status_name} = http_status;
+    let status_line = format!("HTTP/1.1 {status_code} {status_name}");
+    let contents = fs::read_to_string(html_file_path).unwrap();
 
     let length = contents.len();
     let content_length = format!("Content-Length: {length}");
     let headers = format!("{content_length}");
 
     let response = format!("{status_line}\r\n{headers}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap()
+    response
+}
+
+const OK: HttpStatus = HttpStatus { status_code: 200, status_name: "OK" };
+const NOT_FOUND: HttpStatus = HttpStatus { status_code: 404, status_name: "NOT_FOUND" };
+
+struct HttpStatus<'a> {
+    status_code: u64,
+    status_name: &'a str,
 }
